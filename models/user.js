88f4d5ac788a48cf371dd5ac5dc226ba57403575
Maurine karaba <karabawangechi@gmail.com>
2024-07-22 10:23:13 +0300

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const UserSchema = new Schema({
+    
+    firstname:{
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    
+    password: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now()
+    }
+})
+UserSchema.methods.isValidPassword = function (password) {
+    // Simple password validation, you can replace it with your own logic
+    return this.password === password;
+};
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports=User
